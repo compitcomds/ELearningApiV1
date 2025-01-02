0700b98d2e4089320f20b9aa91ccbbff9b606295
DarshanJain <compitcomds@gmail.com>
2025-01-03 01:39:37 +0530

    plugin update

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,33 @@
-export default () => ({});
+// config/plugins.ts
+export default ({ env }) => ({
+    "gen-types": {
+      enabled: true,
+      config: {
+        outputLocation: "myTypes.ts",
+        // If this is true, then the outputLocation should be the location to a .ts file
+        singleFile: true,
+      },
+    },
+    'protected-populate': {
+      enabled: true,
+      config: {
+        ['auto-populate']: true,
+      },
+    },
+    upload: {
+      config: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_CLOUD_NAME'),
+          api_key: env('CLOUDINARY_API_KEY'),
+          api_secret: env('CLOUDINARY_API_SECRET'),
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+    
+  });
---
+++ myTypes.ts
@@ -0,0 +1,77 @@
+
+export interface Media {
+  id: number;
+  name: string;
+  alternativeText: string;
+  caption: string;
+  width: number;
+  height: number;
+  formats: { thumbnail: MediaFormat; small: MediaFormat; medium: MediaFormat; large: MediaFormat; };
+  hash: string;
+  ext: string;
+  mime: string;
+  size: number;
+  url: string;
+  previewUrl: string;
+  provider: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface MediaFormat {
+  name: string;
+  hash: string;
+  ext: string;
+  mime: string;
+  width: number;
+  height: number;
+  size: number;
+  path: string;
+  url: string;
+}
+
+export interface User {
+  id?: number;
+  username: string;
+  email: string;
+  provider?: string;
+  confirmed?: boolean;
+  blocked?: boolean;
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+  role: Role | null | number;
+};
+
+export interface Role {
+  id?: number;
+  documentId?: string;
+  createdAt?: Date | string;
+  updatedAt?: Date | string;
+  name: string;
+  description: string;
+  type: string;
+};
+
+export interface FindOne<T> {
+  data: T;
+  meta: {
+    pagination?: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    }
+  };
+};
+
+export interface FindMany<T> {
+  data: T[];
+  meta: {
+    pagination?: {
+      page: number;
+      pageSize: number;
+      pageCount: number;
+      total: number;
+    }
+  };
+};
