b8f2c4dd28115806131398c4c9ad0a5bb5d33411
DarshanJain <compitcomds@gmail.com>
2025-01-05 02:12:20 +0530

    email and some update

--- src/index.ts
+++ src/index.ts
@@ -18,3 +18,53 @@ export default {
    */
   bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
 };
+
+
+
+// async bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {
+//   try {
+//     await strapi
+//       .plugin("email-designer-5")
+//       .service("email")
+//       .sendTemplatedEmail(
+//         {
+//           // required
+//           // This can also be an array of email addresses
+//           to: "to@example.com",
+//           // Optional
+//           cc: ["2003jaindarshan@gmail.com", "compitcomds@gmail.com"],
+//           // Optional
+//           bcc: ["deedraft0@gmail.com"],
+//           // optional if /config/plugins.js -> email.settings.defaultFrom is set
+//           from: "sahayta52@gmail.com",
+//           // optional if /config/plugins.js -> email.settings.defaultReplyTo is set
+//           replyTo: "info@compitcom.com",
+//           // optional array of files
+//           attachments: [],
+//         },
+//         {
+//           // required - Ref ID defined in the template designer (won't change on import)
+//           templateReferenceId: 1,
+//           // If provided here will override the template's subject.
+//           // Can include variables like `Thank you for your order {{= USER.firstName }}!`
+//           subject: `this is testing`,
+//         },
+//         {
+//           // this object must include all variables you're using in your email template
+//           USER: { firstName: "John", lastName: "Doe" },
+//           order: {
+//             products: [
+//               { name: "Article 1", price: 9.99 },
+//               { name: "Article 2", price: 5.55 },
+//             ],
+//           },
+//           shippingCost: 5,
+//           total: 20.54,
+//         }
+//       );
+//     strapi.log.info("Email sent");
+//   } catch (error) {
+//     strapi.log.error(error);
+//   }
+// },
+// };
